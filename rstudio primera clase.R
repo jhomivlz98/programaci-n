103712fc90a1fdcdd6b64dac9834b86f85a624fd
ericcson <ericcsonemmanuel@gamil.com>
2019-09-12 11:11:10 -0500

    primera clase

---
+++ rstudio primera clase.R
@@ -0,0 +1,42 @@
+rm(list=ls())
+getwd()
+setwd()
+dir()
+a<-1
+b=2 
+5-> C
+ls()
+help(ls)
+?ls
+history()
+install.packages("tidiverse")
+install.packages("tidiverse")
+a=6
+b="abc"
+c= F
+class(a)
+vector = c(a,b,c)
+class(a,b,c)
+vector
+vector2 =c("a","a","b","c","c","c")
+factor(vector2)
+v2=c("A",
+df=data.frame()
+d_vino = read.csv()
+d_vino [1:6,c(1,4,6)]
+
+d_video = read.delim("C:/Users/LABNODO17/Desktop/c1_r_peliculas_hollywood.csv", sep = ",")
+nrow(d_video)
+# Cantidad de columnas
+ncol(d_video)
+print(d_video$Budget)
+d_video$Budg1t[10]
+## Nombre de columnas
+colnames(d_video)
+d_video=
+head(d_video)
+V_flag_sony= d_video$leadstudio=="sony"
+V_flag_sony
+sum(d_video["Movie"]=="The Tourist")
+d_video[d_video["Movie"]=="The Tourist","Genre"]="Comedy"
+
