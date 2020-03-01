669972b18ce0a22a66c1b8edb85c350572733d8b
jhomira loja zumaeta <jhomilz29@hotmail.com>
2020-03-01 18:33:03 -0500

    comentario

---
+++ archivo1.py
@@ -0,0 +1,18 @@
+#Este es un comentario
+#Ejercicio
+# edad=int(input("Ingresame tu edad:"))
+
+# if edad>18:
+# 	print("Eres mayor de edad")
+# else:
+# 	print("Eres menor de edad")
+
+
+#Suma
+a=2
+i=0
+if a==2:
+	i+=1 #igual a i=i+1
+	while(i>=1):
+		print("ilimitado")
+		break
